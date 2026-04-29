56932abb1ee5fbbd50c88148ef99bbdd488f17cf
Wandile <wandilen158@gmail.com>
2026-04-29 17:11:32 +0000

    refactor: streamline Slack notification logic and finalize anomaly detection engine for production

--- detector/test_slack.py
+++ detector/test_slack.py
@@ -1,11 +1,6 @@
 from notifier import SlackNotifier
-import yaml
 
-
-with open("config.yaml") as f:
-    config = yaml.safe_load(f)
-
-notifier = SlackNotifier(config["slack_webhook_url"])
+notifier = SlackNotifier()
 
 print("Sending test alert...")
 notifier.send_alert("System is online! Wandile's Anomaly Engine is officially loud and clear.")
